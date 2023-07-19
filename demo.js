fe9c69a5eb7a0d03278e4ecedaecf8da265a66d4
c4ei <c4ei.net@gmail.com>
2023-07-19 14:32:51 +0900

    s8

---
+++ demo.js
@@ -0,0 +1,43 @@
+/*
+cd /home/dev/www/facebook/s8
+node demo.js
+*/
+
+const { Client } = require('@threadsjs/threads.js');
+// import { Client } from '../src/threads.js';
+// import dotenv from 'dotenv';
+// ######################################################################
+const mysql = require('mysql2/promise'); // mysql2 모듈 사용
+// const bitcoin = require('bitcoinjs-lib');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+const dotenv = require('dotenv');
+dotenv.config();
+// MySQL 데이터베이스 연결 설정
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+};
+const pool = mysql.createPool(dbConfig);
+// ######################################################################
+
+(async () => {
+	const client = new Client();
+	// You can also specify a token: const client = new Client({ token: 'token' });
+	// await client.login(process.env.USER_NAME, process.env.PASSWORD);
+	// await client.users.fetch(25025320).then(user => {
+	// 	console.log(user);
+	// });
+
+    // await client.feeds.fetch("_kim_mo_mo_").then(user => {
+    //     console.log(user);
+	// });
+    // client.feeds.fetchThreads
+
+    await client.users.search("_kim_mo_mo_", 10).then(user => {
+        console.log(user);
+    });
+
+})();
